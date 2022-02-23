9670112b54609c1ee138a8e9dac18087d36072e6
László Tarnai <laszlo.tarnai@LEGO.com>
2022-02-23 11:57:11 +0100

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 // src/App.js
 import './App.css';
+import { useState } from 'react';
 import foods from './foods.json';
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import { Footer } from 'antd/lib/layout/layout';
 
 function App() {
-  return <div className="App"></div>;
+  const [getFoods, setFoods] = useState(foods);
+
+  return (
+    <div className="App">
+      {getFoods.map((food, key) => {
+        return (
+          <div key={key}>
+            <p> {food.name} </p>
+            <img src={food.image} alt={food.name} width={200} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 export default App;
